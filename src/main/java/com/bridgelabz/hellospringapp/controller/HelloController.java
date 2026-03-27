9f32372449e9c9f99571c96950e96d0d768971d9
Barnamwork <barnamwork@gmail.com>
2026-03-27 23:26:40 +0530

    [Barnam] UC3: Path Variable API

--- src/main/java/com/bridgelabz/hellospringapp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/hellospringapp/controller/HelloController.java
@@ -17,4 +17,10 @@ public class HelloController {
     public String sayHelloQuery(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+
+    // UC3
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
